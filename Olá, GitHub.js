60baf76663ee2d9f7adc02bb960e7dacab3a77fc
Emerson Sena <62801061+Emerson53na@users.noreply.github.com>
2020-04-02 11:14:30 -0300

    Aula 001

---
+++ Olá, GitHub.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro progrmar...</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            font: normal 25pt Arial;
+        }
+    </style>
+</head>
+<body>
+    <h1>Olá, GitHub!</h1>
+    <p>Estou aprendendo JavaScript</p>
+    <script>
+        window.alert('Minha primeira mensagem.')
+    </script> 
+</body>
